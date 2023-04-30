0b1dbb5842be93abcdd69614f44cc758159049ee
Soeun_Jang <azulcielo_@naver.com>
2023-04-30 17:07:09 +0900

    animal.py 수정

--- animal.py
+++ animal.py
@@ -1,16 +1,20 @@
 class Animal:
-    def __init__(self):
-        pass
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+        
     def speak(self):
         print("The animal speaks.")
         
 class Dog(Animal):   
     def speak(self):
-        print("Woof!")
+        print(f"Hi! I'm {self.name} and {self.age}years. 멍멍")
         
 class Cat(Animal):
     def speak(self):
-        print("Meow!")
+        print(f"Hi! I'm {self.name} and {self.age}years. 냥냥")
 
-my_dog = Dog()
+my_dog = Dog("Happy", 3)
 my_dog.speak()
+my_cat = Cat("Nabi", 2)
+my_cat.speak()
